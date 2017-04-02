03ba4a281065c0af7309ad6d46ed9eb6c7cfb91c
krachzack <hello@phstadler.com>
2017-04-02 16:27:09 +0200

    Added crude version of flying mechanics

--- Kunstharz/Assets/Scripts/Controls.cs
+++ Kunstharz/Assets/Scripts/Controls.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Controls : MonoBehaviour {
 
 	public float rotationSensitivity = 10.0f;
+	public float flyCooldownTime = 0.2f;
 
 	private Camera cam;
+	private float remainingFlyCooldown;
 
 	// Use this for initialization
 	void Start () {
@@ -15,17 +17,38 @@ public class Controls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		HandleRotationInput ();
+		HandleFlyInput ();
+		Debug.DrawRay (transform.position, transform.forward*1000, Color.green);
+	}
+
+	void HandleRotationInput() {
 		float horizontal = Input.GetAxis ("Mouse X");
 		float vertical = Input.GetAxis ("Mouse Y");
-
-		print (horizontal);
 
 		Vector3 angles = transform.rotation.eulerAngles;
 		angles.x += -vertical * rotationSensitivity;
 		angles.y += horizontal * rotationSensitivity;
 		angles.z = 0.0f;
 		transform.rotation = Quaternion.Euler(angles);
+	}
 
-		//cam.transform.Translate (new Vector3 (0f, 0f, -0.01f));
+	void HandleFlyInput() {
+		if (remainingFlyCooldown > 0) {
+			remainingFlyCooldown -= Time.deltaTime;
+		} else if (Input.GetMouseButton (0)) {
+			Fly ();
+		}
+	}
+		
+
+	void Fly() {
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit)) {
+			transform.position = hit.point + hit.normal * 2;
+			transform.forward = hit.normal;
+
+			remainingFlyCooldown = flyCooldownTime;
+		}
 	}
 }
