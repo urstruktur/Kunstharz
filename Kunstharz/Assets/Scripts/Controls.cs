03a019dfb494e97afca5435cba904ee43807e334
krachzack <hello@phstadler.com>
2017-04-02 16:07:39 +0200

    Added primitive orientation input

---
+++ Kunstharz/Assets/Scripts/Controls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+	public float rotationSensitivity = 10.0f;
+
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponentInChildren<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Mouse X");
+		float vertical = Input.GetAxis ("Mouse Y");
+
+		print (horizontal);
+
+		Vector3 angles = transform.rotation.eulerAngles;
+		angles.x += -vertical * rotationSensitivity;
+		angles.y += horizontal * rotationSensitivity;
+		angles.z = 0.0f;
+		transform.rotation = Quaternion.Euler(angles);
+
+		//cam.transform.Translate (new Vector3 (0f, 0f, -0.01f));
+	}
+}
