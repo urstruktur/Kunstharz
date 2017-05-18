e154a595a27fce8b747b9a834e5b0ca3afbdb83e
krachzack <hello@phstadler.com>
2017-05-18 12:14:34 +0200

    Level switching is now crudely animated

--- Kunstharz/Assets/Scripts/Menu&UI/LevelSwitcher.cs
+++ Kunstharz/Assets/Scripts/Menu&UI/LevelSwitcher.cs
@@ -5,23 +5,56 @@ using UnityEngine;
 public class LevelSwitcher : MonoBehaviour {
 
 	public int selectedLevelIdx = 0;
+	public float switchLevelOffset = 10.0f;
+	public float levelSwitchTime = 0.8f;
+
+	private float remainingLevelSwitchTime = 0.0f;
+
+	private int futureSelectedLevelIdx;
+
+	private bool isSwitchingLevel {
+		get {
+			return futureSelectedLevelIdx != selectedLevelIdx;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
+		futureSelectedLevelIdx = selectedLevelIdx;
 		for (int i = 0; i < transform.childCount; ++i) {
 			transform.GetChild (i).gameObject.SetActive (i == selectedLevelIdx);
 		}
 	}
 
-	void SelectLevel(int idx) {
-		transform.GetChild (selectedLevelIdx).gameObject.SetActive (false);
-		transform.GetChild (idx).gameObject.SetActive (true);
-		selectedLevelIdx = idx;
+	void StartLevelSwitchTo(int idx) {
+		if (!isSwitchingLevel && idx != selectedLevelIdx) {
+			futureSelectedLevelIdx = idx;
+
+			var oldLevel = transform.GetChild (selectedLevelIdx);
+			var newLevel = transform.GetChild (futureSelectedLevelIdx);
+
+			newLevel.gameObject.SetActive (true);
+
+			Vector3 offset = Vector3.up * switchLevelOffset;
+
+			Vector3 oldLevelStartPos = transform.GetChild (selectedLevelIdx).position;
+			Vector3 oldLevelEndPos = oldLevelStartPos + offset;
+			Vector3 newLevelStartPos = oldLevelStartPos - offset;
+			Vector3 newLevelEndPos = oldLevelStartPos;
+
+
+			newLevel.position = newLevelStartPos;
+			LeanTween.move (newLevel.gameObject, newLevelEndPos, levelSwitchTime);
+			LeanTween.move (oldLevel.gameObject, oldLevelEndPos, levelSwitchTime).setOnComplete(() => {
+				selectedLevelIdx = idx;
+				oldLevel.gameObject.SetActive(false);
+			});
+		}
 	}
 
 	void NextLevel() {
 		int nextLevelIdx = (selectedLevelIdx + 1) % transform.childCount;
-		SelectLevel (nextLevelIdx);
+		StartLevelSwitchTo (nextLevelIdx);
 	}
 
 	void PrevLevel() {
@@ -31,15 +64,19 @@ public class LevelSwitcher : MonoBehaviour {
 			prevLevelIdx = transform.childCount - 1;
 		}
 
-		SelectLevel (prevLevelIdx);
+		StartLevelSwitchTo (prevLevelIdx);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			PrevLevel ();
-		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			NextLevel ();
+		if (isSwitchingLevel) {
+
+		} else {
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				PrevLevel ();
+			} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				NextLevel ();
+			}
 		}
 	}
 }
