be1075e151c94a8b2a7a64d0068d4fba4a539b83
Andreas Haslinger <haslingerandreas@gmail.com>
2017-05-23 19:52:24 +0200

    [ADDED] start game menu can no be traversed fully

--- Kunstharz/Assets/Scripts/Menu&UI/Menu.cs
+++ Kunstharz/Assets/Scripts/Menu&UI/Menu.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Colorful;
+using TMPro;
 
 public class Menu : MonoBehaviour {
 
 	[Header("Menu Text Objects")]
-	public GameObject [] textObjects = new GameObject[3];
-
-	[Header("Menu Blur Objects")]
-	public GameObject [] blurObjects = new GameObject[3];
-
+	public GameObject [] textObjects;
+	public GameObject [] imageObjects;
+	GameObject [] blurObjects;
 	[Header("Other Parameters")]
 	public GameObject generalCanvas;
-	public GameObject topLevelMenu;
-	public GameObject pickNameMenu;
+	public GameObject effectCanvas;
+	public GameObject [] menus;
+	public GameObject startGameInputFieldBlur;
+	public GameObject startGameInputFieldText;
+	public GameObject startGameInputFieldPlaceholder;
+	public GameObject startGameStartGameButton;
+	public Kunstharz.Publisher publisher;
+	public Kunstharz.Finder finder;
 
-	float [] blurObjectLength = {232f, 499f, 219f};
+
 	float turnXOld = 0;
 	float turnYOld = 0;
 
@@ -46,8 +51,13 @@ public class Menu : MonoBehaviour {
     [FMODUnity.EventRef]
     public string zoomOut = "event:/ui/click";
 
-	public Kunstharz.Publisher publisher;
-	public Kunstharz.Finder finder;
+	void Start() {
+		blurObjects = new GameObject [effectCanvas.transform.childCount];
+		for (int i = 0; i < effectCanvas.transform.childCount; i++) {
+			blurObjects[i] = effectCanvas.transform.GetChild(i).gameObject;
+		}
+
+	}
 
 	void Update() {
 		turnGameWorld ();
@@ -57,7 +67,7 @@ public class Menu : MonoBehaviour {
 	private void UpdateBeacon() {
 		// Updates the beacon to be sent when publishing game on the network
 		if (publisher.enabled) {
-			string beacon = pickNameMenu.GetComponentInChildren<InputField> ().text;
+			string beacon = menus[2].GetComponentInChildren<TMP_InputField> ().text;
 			publisher.beacon = beacon;
 		}
 	}
@@ -72,23 +82,114 @@ public class Menu : MonoBehaviour {
 		turnYOld = mouseYN ();
 	}
 
-	public void Enter(int i) {
-        
-		for (int j = 0; j < 3; j++) {
-			textObjects [j].GetComponent<Text> ().fontSize = 43;
-			blurObjects [j].GetComponent<RawImage>().enabled = false;
-		}
-			
-		textObjects [i].GetComponent<Text> ().fontSize = 122;
-
-		//Set blur
-		blurObjects [i].GetComponent<RawImage>().enabled = true;
-		float height = blurObjects [i].GetComponent<RectTransform> ().rect.height;
-		blurObjects [i].GetComponent<RectTransform> ().sizeDelta = new Vector2(0, height);
-		LeanTween.size (blurObjects [i].GetComponent<RectTransform> (), new Vector2(blurObjectLength [i],height), 0.25f).setEase(LeanTweenType.easeInOutQuint);
-
+	public void _EnterGeneral() {
+		menus[2].GetComponentInChildren<TMP_InputField> ().DeactivateInputField();
         FMODUnity.RuntimeManager.PlayOneShot(hover, Camera.main.transform.position);
     }
+
+	public void _EnterText(GameObject obj) {
+		for (int j = 0; j < textObjects.Length; j++) {
+			if (textObjects [j].GetComponent<Text> () == null) {
+				textObjects [j].GetComponent<TextMeshProUGUI>().fontSize = 52;
+				startGameInputFieldPlaceholder.GetComponent<TextMeshProUGUI>().fontSize = 52;
+			} else {
+				textObjects [j].GetComponent<Text> ().fontSize = 43;
+			}
+		}
+		
+		if (obj.GetComponent<Text> () == null) {
+			obj.GetComponent<TextMeshProUGUI>().fontSize = 130;
+			startGameInputFieldPlaceholder.GetComponent<TextMeshProUGUI>().fontSize = 130;
+		} else {
+			obj.GetComponent<Text> ().fontSize = 122;
+		}
+			
+	}
+
+	public void _EnterText() {
+		for (int j = 0; j < textObjects.Length; j++) {
+			if (textObjects [j].GetComponent<Text> () == null) {
+				textObjects [j].GetComponent<TextMeshProUGUI>().fontSize = 52;
+				startGameInputFieldPlaceholder.GetComponent<TextMeshProUGUI>().fontSize = 52;
+			} else {
+				textObjects [j].GetComponent<Text> ().fontSize = 43;
+			}
+		}	
+	}
+
+	public void _EnterBlur(GameObject obj) {
+		for (int j = 0; j < blurObjects.Length; j++) {
+			blurObjects [j].GetComponent<RawImage>().enabled = false;
+		}
+
+		obj.GetComponent<RawImage>().enabled = true;
+		float height = obj.GetComponent<RectTransform> ().rect.height;
+		float width = obj.GetComponent<RectTransform>().rect.width;
+		obj.GetComponent<RectTransform> ().sizeDelta = new Vector2(0, height);
+		LeanTween.size (obj.GetComponent<RectTransform> (), new Vector2(width,height), 0.25f).setEase(LeanTweenType.easeInOutQuint);
+	}
+
+	public void _EnterBlur() {
+		for (int j = 0; j < blurObjects.Length; j++) {
+			blurObjects [j].GetComponent<RawImage>().enabled = false;
+		}
+	}
+
+	public void _EnterImage(GameObject obj) {
+		for (int j = 0; j < imageObjects.Length; j++) {
+			imageObjects [j].GetComponent<RectTransform>().localScale = new Vector3(0.3f, 0.3f, 0.3f);
+		}
+		
+		obj.GetComponent<RectTransform>().localScale = new Vector3(0.7f, 0.7f, 0.7f);
+	}
+
+	public void _EnterImage() {
+		for (int j = 0; j < imageObjects.Length; j++) {
+			imageObjects [j].GetComponent<RectTransform>().localScale = new Vector3(0.3f, 0.3f, 0.3f);
+		}
+	}
+
+	private float mouseXN() {
+		return Input.mousePosition.x / (float) Screen.width * 2f - 1f;
+	}
+
+	private float mouseYN() {
+		return Input.mousePosition.y / (float) Screen.height * 2f - 1f;
+	}
+
+	public void OnValueChangeInputFieldGameStart(string s) {
+
+		if (s != "") {
+			startGameStartGameButton.SetActive(true);
+		} else {
+			startGameStartGameButton.SetActive(false);
+			s = "TYPE YOUR NAME";
+		}
+
+		float width = startGameInputFieldText.GetComponent<TextMeshProUGUI>().GetPreferredValues(s).x + 100;
+		float height = startGameInputFieldBlur.GetComponent<RectTransform> ().rect.height;
+
+		startGameInputFieldBlur.GetComponent<RectTransform> ().sizeDelta = new Vector2 (width, height);
+		LeanTween.size (startGameInputFieldBlur.GetComponent<RectTransform> (), new Vector2(width,height), 0.25f).setEase(LeanTweenType.easeInOutQuint);
+	}
+
+	private void SetMenusInactive() {
+		for (int i = 0; i < menus.Length; i++) {
+			menus[i].SetActive(false);
+		}
+
+		for (int i = 0; i < blurObjects.Length; i++) {
+			blurObjects [i].GetComponent<RawImage>().enabled = false;
+		}
+	}
+
+	public void TopLevelMenu() {
+		publisher.enabled = false;
+		finder.enabled = false;
+
+		SetMenusInactive();
+		menus[0].SetActive (true);
+	}
 
 	public void ExitGame () {
 		Application.Quit();
@@ -101,34 +202,23 @@ public class Menu : MonoBehaviour {
     }
 
 	public void JoinGame () {
-		//TODO: Go to join scene
-		Debug.Log("TODO: Start Join Game Menu");
-
 		finder.enabled = true;
 		publisher.enabled = false;
-
         FMODUnity.RuntimeManager.PlayOneShot(click, Camera.main.transform.position);
     }
 
-	public void StartGame () {
-		//TODO: Go to start scene
-		Debug.Log("TODO: Start Start Game Menu");
+	public void StartGameChooseLevelMenu () {
+		SetMenusInactive();
+		menus[1].SetActive (true);
+        FMODUnity.RuntimeManager.PlayOneShot(click, Camera.main.transform.position);
+    }
 
-		topLevelMenu.SetActive (false);
-		pickNameMenu.SetActive (true);
-
+	public void StartGameChooseNameMenu() {
 		publisher.enabled = true;
 		finder.enabled = false;
-
-        FMODUnity.RuntimeManager.PlayOneShot(click, Camera.main.transform.position);
-    }
-
-	private float mouseXN() {
-		return Input.mousePosition.x / (float) Screen.width * 2f - 1f;
-	}
-
-	private float mouseYN() {
-		return Input.mousePosition.y / (float) Screen.height * 2f - 1f;
+		SetMenusInactive();
+		menus[2].SetActive (true);
+		FMODUnity.RuntimeManager.PlayOneShot(click, Camera.main.transform.position);
 	}
 
 }
