5e55eb7fc72c1645d8a8b690f99577b0fca1e708
Andreas Haslinger <haslingerandreas@gmail.com>
2017-05-31 15:02:20 +0200

    [RESOLVE] error

--- Kunstharz/Assets/Scripts/Menu/Menu.cs
+++ Kunstharz/Assets/Scripts/Menu/Menu.cs
@@ -88,55 +88,5 @@ public class Menu : MonoBehaviour {
 	private float mouseYN() {
 		return Input.mousePosition.y / (float) Screen.height * 2f - 1f;
 	}
-<<<<<<< HEAD
 	
-=======
-
-	private void SetMenusInactive() {
-		for (int i = 0; i < menus.Length; i++) {
-			menus[i].SetActive(false);
-		}
-		blur.GetComponent<RawImage>().enabled = false;
-	}
-
-	public void TopLevelMenu() {
-		publisher.enabled = false;
-		finder.enabled = false;
-
-		SetMenusInactive();
-		menus[0].SetActive (true);
-	}
-
-	public void JoinGame () {
-		finder.enabled = true;
-		publisher.enabled = false;
-    }
-
-	public void StartGameChooseLevelMenu () {
-		SetMenusInactive();
-		menus[1].SetActive (true);
-    }
-
-	public void ExitGame () {
-		Application.Quit();
-
-		#if UNITY_EDITOR
-		UnityEditor.EditorApplication.isPlaying = false;
-		#endif
-    }
-
-	public void StartGameChooseNameMenu() {
-		publisher.enabled = true;
-		finder.enabled = false;
-		SetMenusInactive();
-		menus[2].SetActive (true);
-	}
-
-    public void StartGameWaiting()
-    {
-        SetMenusInactive();
-        menus[3].SetActive(true);
-		loader.StartUgly (selectedLevelIdx);
-    }
->>>>>>> 2b2472212575a602208693fa948a223f023938f0
 }
