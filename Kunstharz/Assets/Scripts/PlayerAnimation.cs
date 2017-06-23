251a27c362f3ecd27913c49037fe36b2c1c5548d
Sarah Sauseng <sarahsauseng@Sarahs-MacBook-Pro.local>
2017-06-23 16:17:56 +0200

    Added script for animation and update fix models

---
+++ Kunstharz/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	public GameObject diamond;
+
+	public GameObject tank;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LeanTween.move(diamond, new Vector3(1,1,1), 5.0f);
+	}
+}
