e0403d58fe8b0cd5f493689f6e3732b4c9a9aab8
Sarah Sauseng <sarahsauseng@Sarahs-MacBook-Pro.local>
2017-06-23 19:06:48 +0200

    Added base pulse animation for diamond

--- Kunstharz/Assets/Scripts/PlayerAnimation.cs
+++ Kunstharz/Assets/Scripts/PlayerAnimation.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour {
 
 	public GameObject diamond;
-
 	public GameObject tank;
+
+	private const float D_TIMER = 4f;
+	private const float T_TIMER = 6f;
+
+	private float diamondTimer = D_TIMER;
+	private float tankTimer = T_TIMER;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +20,21 @@ public class PlayerAnimation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		LeanTween.move(diamond, new Vector3(1,1,1), 5.0f);
+		diamondTimer -= Time.deltaTime;
+
+		if(diamondTimer <= 0) {
+			PerformPulseAnimation();
+			diamondTimer = D_TIMER;
+		}
+	}
+
+	private void PerformPulseAnimation () {
+		LeanTween.rotateAround(diamond, Vector3.up, 360f, 0.05f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
+			LeanTween.rotateZ(diamond, 1f, 0.05f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
+				LeanTween.rotateZ(diamond, -5f, 0.05f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
+					LeanTween.rotateZ(diamond, 0f, 0.05f).setEase(LeanTweenType.easeInOutQuad);
+				}).setLoopPingPong(7);
+			});
+		});
 	}
 }
