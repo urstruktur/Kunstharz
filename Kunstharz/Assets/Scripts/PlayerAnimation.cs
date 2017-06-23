80771c8df45894fcac6b23dec4da69e9b6cb0af9
Sarah Sauseng <sarahsauseng@Sarahs-MacBook-Pro.local>
2017-06-23 21:03:21 +0200

    Add basic tank animation

--- Kunstharz/Assets/Scripts/PlayerAnimation.cs
+++ Kunstharz/Assets/Scripts/PlayerAnimation.cs
@@ -21,19 +21,31 @@ public class PlayerAnimation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		diamondTimer -= Time.deltaTime;
+		tankTimer -= Time.deltaTime;
 
 		if(diamondTimer <= 0) {
 			PerformPulseAnimation();
 			diamondTimer = D_TIMER;
 		}
+
+		if(tankTimer <= 0) {
+			PerformTankMechanicAnimation();
+			tankTimer = T_TIMER;
+		}
 	}
 
 	private void PerformPulseAnimation () {
-		LeanTween.rotateAround(diamond, Vector3.up, 360f, 0.05f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
-			LeanTween.rotateZ(diamond, 5f, 0.05f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
+		LeanTween.rotateZ(diamond, 5f, 0.05f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
 				LeanTween.rotateZ(diamond, -5f, 0.05f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
 					LeanTween.rotateZ(diamond, 0f, 0.05f).setEase(LeanTweenType.easeInOutQuad);
 				}).setLoopPingPong(7);
+			});
+	}
+
+	private void PerformTankMechanicAnimation () {
+		LeanTween.scale(tank, new Vector3(1.2f, 1f, 1.2f), 0.1f).setEase(LeanTweenType.easeOutBack).setOnComplete(() => {
+			LeanTween.rotateAround(tank, Vector3.up, 360f, 0.75f).setEase(LeanTweenType.easeInOutBack).setOnComplete(() => {
+				LeanTween.scale(tank, new Vector3(1f, 1f, 1f), 0.1f).setEase(LeanTweenType.easeOutBack);
 			});
 		});
 	}
