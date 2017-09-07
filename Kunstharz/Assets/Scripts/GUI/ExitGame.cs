de3caa596e6743268bf0dd3c55252ebcfea310f6
Andreas Haslinger <haslingerandreas@gmail.com>
2017-09-07 15:36:13 +0200

    [ADDED] press escape to exit game 👖

---
+++ Kunstharz/Assets/Scripts/GUI/ExitGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Cursor.lockState = CursorLockMode.Confined;
+			print("loading menu");
+			NetworkManager.Shutdown();
+			SceneManager.LoadScene(0);
+		}
+	}
+}
