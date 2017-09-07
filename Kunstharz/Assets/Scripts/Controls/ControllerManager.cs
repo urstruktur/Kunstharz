0095608687a6ca277c5c51a65c2ea9096bd5e4cc
Sarah Sauseng <sarahsauseng@Sarahs-MBP.home>
2017-09-07 14:42:29 +0200

    Added test script with basic behaviour & base scripts for separating parts.

---
+++ Kunstharz/Assets/Scripts/Controls/ControllerManager.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+using InControl;
+
+public class ControllerManager : MonoBehaviour {
+
+}
---
+++ Kunstharz/Assets/Scripts/Controls/InControlTestScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using InControl;
+
+namespace Kunstharz {
+	public class InControlTestScript : MonoBehaviour {
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+            // get active device
+            InputDevice device = InputManager.ActiveDevice; 
+            
+            // map certain part of device
+            InputControl control = device.GetControl(InputControlType.LeftStickUp);
+
+            // if left stick moved
+            if(control.HasChanged){
+                Debug.Log("controller moved");
+            }
+
+            // Checking devices
+            InputManager.OnDeviceAttached += inputDevice => Debug.Log( "Attached: " + inputDevice.Name );
+            InputManager.OnDeviceDetached += inputDevice => Debug.Log( "Detached: " + inputDevice.Name );
+            InputManager.OnActiveDeviceChanged += inputDevice => Debug.Log( "Switched: " + inputDevice.Name );
+        }
+
+    }
+    
+}
